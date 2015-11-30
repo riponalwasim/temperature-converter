f666344bf4234f8bf6c324fa951288e0c44c2781
riponalwasim <riponalwasim@gmail.com>
2015-11-29 21:39:56 -0800

    modified step definitions

--- src/test/java/com/packtpub/mastering/selenium/steps/ConversionStepDefs.java
+++ src/test/java/com/packtpub/mastering/selenium/steps/ConversionStepDefs.java
@@ -38,6 +38,7 @@ public class ConversionStepDefs {
 
     @Then("^It should be converted to Celsius as (\\d+) degree$")
     public void It_should_be_converted_to_Celsius_as_degree(int arg1) throws Throwable {
+        //assertion
         assertEquals(temperatureConverterPage.actualResult(), temperatureConverterPage.expectedResult());
         driver.quit();
     }
